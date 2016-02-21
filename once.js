016bc5c19666c72a964b464ab4454e933740a9ff
Zoltan Kochan <zoltan.kochan@gmail.com>
2016-02-22 00:55:23 +0200

    once.strict added

--- once.js
+++ once.js
@@ -1,4 +1,16 @@
 var wrappy = require('wrappy')
+var once = secondCallHandler(function (f) { return f.value })
+
+once.strict = wrappy(secondCallHandler(function (f) {
+  throw new Error(f.onceError || createErrorMsg(f))
+}))
+
+function createErrorMsg (f) {
+  return f.srcName
+    ? f.srcName + " shouln't be called more than once"
+    : "Function wrapped with `once` shouln't be called more than once"
+}
+
 module.exports = wrappy(once)
 
 once.proto = once(function () {
@@ -8,14 +20,24 @@ once.proto = once(function () {
     },
     configurable: true
   })
+
+  Object.defineProperty(Function.prototype, 'strictOnce', {
+    value: function () {
+      return once.strict(this)
+    },
+    configurable: true
+  })
 })
 
-function once (fn) {
-  var f = function () {
-    if (f.called) return f.value
-    f.called = true
-    return f.value = fn.apply(this, arguments)
+function secondCallHandler (onSecondCall) {
+  return function (fn) {
+    var f = function () {
+      if (f.called) return onSecondCall(f)
+      f.called = true
+      return f.value = fn.apply(this, arguments)
+    }
+    f.srcName = fn.name
+    f.called = false
+    return f
   }
-  f.called = false
-  return f
 }
